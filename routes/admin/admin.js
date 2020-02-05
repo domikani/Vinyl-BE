9af3619c2645bda79e25777c99a9f2501f086068
domna <domnakanari@gmail.com>
2020-02-05 20:03:12 +0200

    statistics

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -8,6 +8,30 @@ route.get("/", AdminAuth, (req, res) => {
         message: "Admin Area"
     });
 });
+
+route.get("/stats", AdminAuth, async (req, res) => {
+    const categories = await Category.find().exec();
+    const labels = categories.map( c => c.title);
+    const counts = [];
+
+    for (let cat of categories){
+        const num= await Product.count({category:cat._id});
+        counts.push(num);
+    }
+
+    res.json({
+        success: true,
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'My categories dataset',
+                    data: counts
+                }
+            ]
+        }
+    });
+});
 //User
 route.use("/auth", require('./auth'));
 
